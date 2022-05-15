e0187936ec87b294c85faf649eb0c33b9d43b232
Jontu Das <jontudas624@gmail.com>
2022-05-16 00:15:07 +0600

    add service section

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,9 @@
 
 const typed = new Typed('.animation', {
 	// Waits 1000ms after typing "First"
-	strings: [
-              'hello',
-	 		  'web Developer',
-               'Font-end Expart'
-			],
-            typeSpeed: 40,
-            backSpeed: 40,
-            loop: true
+	strings: ["Web Developer", "Font-end Expart", "WordPress Expert"],
+    typeSpeed: 40,
+    backSpeed: 40,
+    loop: true
+         
   });
