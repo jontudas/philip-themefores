cbf6bf3bc19623dc273798e788cffca5ea722497
Jontu Das <jontudas624@gmail.com>
2022-05-15 20:47:05 +0600

    add text animation and hover effect

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+// typejs animation 
+
+const typed = new Typed('.animation', {
+	// Waits 1000ms after typing "First"
+	strings: [
+              'hello',
+	 		  'web Developer',
+               'Font-end Expart'
+			],
+            typeSpeed: 40,
+            backSpeed: 40,
+            loop: true
+  });
