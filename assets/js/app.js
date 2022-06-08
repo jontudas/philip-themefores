00b45424f6dbd0a84ba333f65cf3add721ae9201
hemalrika <hemalrika@gmail.cmo>
2022-06-08 20:08:06 +0600

    updated

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// typejs animation 
+
+const typed = new Typed('.animation', {
+	// Waits 1000ms after typing "First"
+	strings: ["Web Developer", "Font-end Expart", "WordPress Expert"],
+    typeSpeed: 40,
+    backSpeed: 40,
+    loop: true
+         
+  });
